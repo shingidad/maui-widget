a680cf87250039940ec904dd15a2d4d1501887f9
shingidad <shingidad@gmail.com>
2017-10-23 15:36:14 +0900

    build 변경, example server port 변경 3000 -> 4000

--- example/server.js
+++ example/server.js
@@ -4,42 +4,6 @@ const express = require('express'),
 const PROT = process.env.PORT || 4000;
 const app = express();
 app.use(bodyParser.json());
-app.get('/widget.json', function (req, res) {
-    res.json({
-        "_id": "demo",
-        "list": [
-            {
-                "_id": "iscs.movienet.kr1",
-                "appType": "fullpage",
-                "group": "video",
-                "url": "https://may-widget.herokuapp.com/",
-                "x": 0,
-                "y": 0,
-                "w": 1920,
-                "h": 1080,
-                "runPattern": "channel",
-                "pattern": "",
-                "NAME": "Example",
-                "CH_NO": "0",
-                "major": 0
-            }, {
-                "_id": "iscs.movienet.kr",
-                "appType": "fullpage",
-                "group": "video",
-                "url": "http://iscs.movienet.kr",
-                "x": 0,
-                "y": 0,
-                "w": 1920,
-                "h": 1080,
-                "runPattern": "channel",
-                "pattern": "",
-                "NAME": "MovieNet",
-                "CH_NO": "1",
-                "major": 1
-            }
-        ]
-    })
-});
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(parseInt(PROT), () => {
